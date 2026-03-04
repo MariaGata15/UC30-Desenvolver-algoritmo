5dd8dd8a07b86122d3cfb661039a1862885e2a8b
MariaGata15 <mariaclarashinbel2021@gmail.com>
2026-03-04 11:19:40 -0300

    Add files via upload

---
+++ uc-algoritmo04-03-26/atividade.py
@@ -0,0 +1,6 @@
+#Crie uma lista chamada COMPRAS com pelo menos 5 itens. Adicione um novo item à lista de que forma que o usuário digite.
+
+lista = ["pão", "bolo", "biscoito", "café", "cuscuz"]
+adicionar=input("adicione um item a lista:")
+lista.append(adicionar)
+print(lista)
